18941c410dbda6e6a748f9ad9e9a5c30939ce576
Yan <dyan@Danhua-iMac.local>
2017-08-10 17:12:34 -0400

    add new version of atom_mass.py

--- atom_mass.py
+++ atom_mass.py
@@ -1,63 +1,63 @@
-#Copyright:Danhua Yan
+# Copyright:Danhua Yan, Jiajie Cen
+# 2017.8.7 Verson 2.0.0
 import re
-def molarmass(molecule):
-    #creat atomic mass dictionary eledict
+
+def split_input(input_name, grams):
+    atom_mass_dict = import_atom()
+    atom = re.findall(r"[A-Z][a-z]*", input_name)
+    numbers = list(map(float, re.findall(r"[0-9.]+", input_name)))
+    grams = float(grams)
+
+    mass = 0
+    for i in atom:
+        mass += atom_mass_dict[i] * numbers[atom.index(i)]
+
+    mol = grams / mass
+
+    atom_mol = dict(zip(atom, [i * mol for i in numbers]))
+    return atom_mol
+
+def import_atom():
     file = open("atomicmass.txt")
-    elelist = list()
-    eledict = dict()
+    atom_mass_dict = dict()
     for line in file:
-        str = line.rstrip()
-        elelist = str.split('\t')
-        eledict[elelist[0]] = elelist[1]
-    #Deconstruct input chemical name into atomic mass.
-    summary = list()
-    #split chemical expression, from codego.net/9145913
-    elementlist = re.findall(r'[A-Z][a-z]*|\d+', re.sub('[A-Z][a-z]*(?![\da-z])', r'\g<0>1', molecule))
-    for name in elementlist:
-        x = eledict.get(name,name)
-        summary.append(x)
-    #Cacl. total molecule molar mass.
-    n = 0
-    tot = 0
-    while n < len(summary):
-        val = float(summary[n]) * float(summary[n+1])
-        n = n + 2
-        tot = tot + val
-    return tot
+        atom = line.split()
+        atom_mass_dict[atom[0]] = float(atom[1])
+    return atom_mass_dict
 
-M1 = 0
-M2 = 0
-n1 = 0
-n2 = 0
-x = 0 #targeted grams in total
-a = 0 #molar rario
 
-while True:
-    try:
-        M1n = raw_input("Name of component 1:")
-        M1 = molarmass(M1n)
-        print "Molar mass of", M1n, "is:",M1
-        M2n = raw_input("Molecular weight of component 2:")
-        M2 = molarmass(M2n)
-        print "Molar mass of", M2n, "is:",M2
 
-        while True:
-            x = raw_input("Please enter weight for your target (in grams):")
-            x = float(x)
-            a = raw_input("Com1:Com2 molar ratio:")
-            a = float(a)
 
-            n2 = x / (a * M1 + M2)
-            n1 = a * n2
+#def molarmass(molecule):
+#    #creat atomic mass dictionary eledict
+#    file = open("atomicmass.txt")
+#    elelist = list()
+#    eledict = dict()
+#    for line in file:
+#        str = line.rstrip()
+#        elelist = str.split('\t')
+#        eledict[elelist[0]] = elelist[1]
+#    #Deconstruct input chemical name into atomic mass.
+#    summary = list()
+#    #split chemical expression, from codego.net/9145913
+#    elementlist = re.findall(r'[A-Z][a-z]*|\d+', re.sub('[A-Z][a-z]*(?![\da-z])', r'\g<0>1', molecule))
+#    for name in elementlist:
+#        x = eledict.get(name,name)
+#        summary.append(x)
+#    #Cacl. total molecule molar mass.
+#    n = 0
+#    tot = 0
+#    while n < len(summary):
+#        val = float(summary[n]) * float(summary[n+1])
+#        n = n + 2
+#        tot = tot + val
+#    return tot
 
-            print "m1 =", n1 * M1
-            print "m2 =", n2 * M2
+if __name__ == '__main__':
+    t_name = input("Please key in the target name:")
+    t_gram = input("Please key in the target gram:")
+    if len(t_name) < 1:
+        t_name = "Ti0.8O1La1Ta7"
 
-            term = raw_input("Enter any button (except 0) to calculate another ratio. Enter 0 to quit.\n")
-            if term == '0':
-                break
-            else: continue
-    except:
-        print "Syntax Error. Please check your input."
-        continue
-    quit()
+    x = split_input(t_name, t_gram)
+    print(x)
